f6fceffea0256dc8804d52c0cf722065b68fb77b
Ahmad Khan <chofman@outoforgedu.onmicrosoft.com>
2023-09-04 13:47:35 -0400

    Enabled Debugging

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript: {
-      ignoreBuildErrors: true,
+    compiler: {
+      removeConsole: false,
     },
     experimental: {
       serverActions: true,
