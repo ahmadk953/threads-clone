65a941f21403916140d0ca8e156e05039ec16cd1
Ahmad Khan <chofman@outoforgedu.onmicrosoft.com>
2023-09-18 17:14:49 -0400

    Fixed some issues

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,40 +1,43 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import '../globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
-import Topbar from '@/components/shared/Topbar'
-import LeftSidebar from '@/components/shared/LeftSidebar'
-import RightSidebar from '@/components/shared/RightSidebar'
-import Bottombar from '@/components/shared/Bottombar'
+import "../globals.css";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: 'Threads',
-  description: 'A Next.js 13 Meta Threads Application'
-}
+export const metadata: Metadata = {
+  title: "Threads",
+  description: "A Next.js 13 Meta Threads application",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang='en'>
         <body className={inter.className}>
           <Topbar />
 
-          <main className="flex flex-row">
+          <main className='flex flex-row'>
             <LeftSidebar />
-
-            <section className="main-container">
-              <div className="w-full max-w-4xl">
-                {children}
-              </div>
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
             </section>
-
+            {/* @ts-ignore */}
             <RightSidebar />
           </main>
 
@@ -42,5 +45,5 @@ export default function RootLayout({
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
