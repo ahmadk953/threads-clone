bbdb712eb01cf943b42425704d5626ad1907f546
Ahmad <103906421+ahmadk953@users.noreply.github.com>
2023-09-18 21:43:21 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,41 +1,34 @@
-// This file was automatically added by edgio init.
-// You should commit this file to source control.
-const { withEdgio } = require('@edgio/next/config')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      serverActions: true,
-      serverComponentsExternalPackages: ["mongoose"],
-    },
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "img.clerk.com",
-        },
-        {
-          protocol: "https",
-          hostname: "images.clerk.dev",
-        },
-        {
-          protocol: "https",
-          hostname: "uploadthing.com",
-        },
-        {
-          protocol: "https",
-          hostname: "placehold.co",
-        },
-      ],
-      typescript: {
-        ignoreBuildErrors: true,
+  experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
       },
-    },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
+  },
 };
 
-const _preEdgioExport = nextConfig;;
-
-module.exports = (phase, config) =>
-  withEdgio({
-    ..._preEdgioExport
-  })
+module.exports = nextConfig;
