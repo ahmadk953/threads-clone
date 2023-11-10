d6facafa13017f9fce83b57094d9fe0a859cc537
Ahmad Khan <chofman@outoforgedu.onmicrosoft.com>
2023-11-10 17:48:52 -0500

    Reverted back to Next.js 13 due to build errors

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,6 +1,3 @@
-// Resource: https://docs.uploadthing.com/nextjs/appdir#create-a-nextjs-api-route-using-the-filerouter
-// Copy paste (be careful with imports)
-
 import { createNextRouteHandler } from "uploadthing/next";
 
 import { ourFileRouter } from "./core";
