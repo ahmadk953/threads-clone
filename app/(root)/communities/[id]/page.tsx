f05709fb850498d9a18b94d9b96eeb4cdba35d2c
Ahmad Khan <chofman@outoforgedu.onmicrosoft.com>
2023-11-22 20:45:09 -0500

    Fixed issues and updated Next.js and Clerk version

--- app/(root)/communities/[id]/page.tsx
+++ app/(root)/communities/[id]/page.tsx
@@ -19,7 +19,7 @@ async function Page({ params }: { params: { id: string } }) {
   return (
     <section>
       <ProfileHeader
-        accountId={communityDetails?.createdBy?.id}
+        accountId={communityDetails.createdBy.id}
         authUserId={user.id}
         name={communityDetails.name}
         username={communityDetails.username}
